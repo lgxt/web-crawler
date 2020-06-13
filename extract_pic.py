39c65f5efb234a3492033b2a6b3be40789e70ca5
FangmingJin <54917042+FangmingJin@users.noreply.github.com>
2020-06-12 22:53:24 -0400

    Update extract_pic.py

--- extract_pic.py
+++ extract_pic.py
@@ -12,7 +12,7 @@ import filetype
 
 def extract_pic(query):
     work_dir = Path.cwd().parent
-    import_html_dir = work_dir / 'try' / query
+    import_html_dir = work_dir / 'html' / query
     dir = work_dir / 'pic'
     if not dir.exists():
         dir.mkdir()
@@ -34,18 +34,27 @@ def extract_pic(query):
     for file in list(import_html_dir.glob('*.html')):
         filename = os.path.basename(file)
         tar_link = work_dir / 'pic' / query / filename.rstrip('.html')
-        if not tar_link.exists():
+        if not os.path.exists(tar_link):
             tar_link.mkdir()
-        with open(file, encoding='utf-8') as f:
-            html_content = f.read()
-        for reg in reg_all:
-            imgre = re.compile(reg)
-            imglist = re.findall(imgre, html_content)
+            with open(file, encoding='utf-8') as f:
+                html_content = f.read()
+            imglist = []
+            # 获得图片链接列表
+            for reg in reg_all:
+                imgre = re.compile(reg)
+                imglist = re.findall(imgre, html_content)+imglist
+            imglist = list(set(imglist)) 
             print(imglist)
             for item in imglist:
-                img_data = requests.get(item, headers=header).content
-                if str(filetype.guess(img_data)).count('Jpeg')>0 or str(filetype.guess(img_data)).count('Gif')>0:
-                    style=re.split(r'[.\s]',str(filetype.guess(img_data)))[3].lower()
-                    with open(os.path.join(tar_link, f'{uuid.uuid4().hex}.{style}'), 'w+') as f:
-                        f.buffer.write(img_data)
-                time.sleep(3)
+                if not item.startswith('http'):
+                    item = f'http://{item}'
+                try: #图片下载，只下载jpeg与gif类
+                    img_data = requests.get(item, headers=header).content
+                    if str(filetype.guess(img_data)).count('Jpeg')>0 or str(filetype.guess(img_data)).count('Gif')>0:
+                        style=re.split(r'[.\s]',str(filetype.guess(img_data)))[3].lower()
+                        with open(os.path.join(tar_link, f'{uuid.uuid4().hex}.{style}'), 'w+') as f:
+                            f.buffer.write(img_data)
+                except:
+                    print('error',filename,item)
+            time.sleep(3) #下载图片不需要限速
+extract_pic('金蝶')
